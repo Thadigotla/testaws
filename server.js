94cfe1bdccd9c7cf935bc446af6f91ca70d62b79
Thadigotla Sreekanth Reddy <sreekanth@ampcome.com>
2022-12-08 15:33:11 +0530

    reinitialized files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res)=>{
+
+
+    res.send('Hello World');
+})
+
+
+app.listen(8081,()=>{
+
+    console.log(`Server running at: http://localhost:8081/`);
+
+});
